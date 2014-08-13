ea804e889c9c035849040172c016a7cabd2bdee6
Sergey <cherrer@mail.ru>
2014-08-13 12:15:54 +0400

    rewrited error handler

--- error_handler.rb
+++ error_handler.rb
@@ -1,10 +1,10 @@
 require './response_error'
 
 class ErrorHandler < StandardError
-	def initialize(parsed)		
-		raise parsed["message"] if parsed["code"] != nil && parsed["code"] != 200
-		return parsed
-	resque
-		return ResponseError.new(parsed["code"], parsed["message"])
+	def initialize(parsed)
+		if parsed["code"] != nil && parsed["code"] != 200	
+			raise ArgumentError.new(parsed["message"])
+			return ResponseError.new(parsed["code"], parsed["message"])
+		end
 	end
 end
--- yandex_translate.rb
+++ yandex_translate.rb
@@ -8,18 +8,35 @@ class YandexTranslate
 	def get_langs(lang = 'en')
 		data = {"ui" => lang}
 		parsed = Connect.new.get('getLangs', data)
-		return ErrorHandler.new(parsed)
+		begin
+			err = ErrorHandler.new(parsed)
+			return parsed
+		rescue ErrorHandler
+			return err
+		end
 	end
 
 	def detect(text)
 		data = {"text" => text}
 		parsed = Connect.new.get('detect', data)
-		return ErrorHandler.new(parsed)
+		begin
+			err = ErrorHandler.new(parsed)
+			return parsed
+		rescue ErrorHandler
+			return err
+		end
 	end
 
 	def translate(text, lang = 'ru', format = 'plain')
 		data = {"text" => text, "lang" => lang, "format" => format}
 		parsed = Connect.new.get('translate', data)
-		return ErrorHandler.new(parsed)
+		begin
+			err = ErrorHandler.new(parsed)
+			return parsed
+		rescue ErrorHandler
+			return err
+		end
 	end
 end
+
+puts YandexTranslate.new.get_langs
